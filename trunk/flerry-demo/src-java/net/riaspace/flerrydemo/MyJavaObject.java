dc606d9900b538739afc613e9f53285a8e31d630
piotr.walczyszyn <piotr.walczyszyn@362a26de-005a-11df-8c0c-f1c8ac664a8e>
2010-01-13 16:25:34 +0000

    - initial commit

---
+++ trunk/flerry-demo/src-java/net/riaspace/flerrydemo/MyJavaObject.java
@@ -0,0 +1,11 @@
+package net.riaspace.flerrydemo;
+
+public class MyJavaObject
+{
+	public MyJavaObject(){}
+	
+	public Integer add(Integer value1, Integer value2)
+	{
+		return value1 + value2;
+	}
+}
