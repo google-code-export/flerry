73d077de7ca9adc45285f547151832c2008def11
piotr.walczyszyn <piotr.walczyszyn@362a26de-005a-11df-8c0c-f1c8ac664a8e>
2010-02-05 08:50:59 +0000

    - Added passing ComplexVO example

--- trunk/flerry-demo/src-java/net/riaspace/flerrydemo/MyJavaObject.java
+++ trunk/flerry-demo/src-java/net/riaspace/flerrydemo/MyJavaObject.java
@@ -1,5 +1,9 @@
 package net.riaspace.flerrydemo;
 
+import java.util.Date;
+
+import net.riaspace.flerrydemo.models.ComplexVO;
+
 public class MyJavaObject
 {
 	public MyJavaObject(){}
@@ -7,5 +11,14 @@ public class MyJavaObject
 	public Integer add(Integer value1, Integer value2)
 	{
 		return value1 + value2;
+	}
+	
+	public ComplexVO processComplexVO(ComplexVO cvo)
+	{
+		cvo.setSomeDate(new Date());
+		cvo.setSomeString("Hello Flex!");
+		cvo.setSomeInteger(1);
+		
+		return cvo;
 	}
 }
